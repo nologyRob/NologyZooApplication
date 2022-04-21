daf7c783d26e67d05f4bdbd9361d54adc1b7d87d
charlie-robin <charlie.robin.richardson@gmail.com>
2022-04-21 17:19:05 +0100

    (feat): Removed Pens & PenCommands

--- src/com/company/Zoo.java
+++ src/com/company/Zoo.java
@@ -14,11 +14,9 @@ import java.util.ArrayList;
 public class Zoo {
 
     private ArrayList<Animal> animals;
-    private ArrayList<Pen<Animal>> pens;
 
     public Zoo() {
         this.animals = new ArrayList<>();
-        this.pens = new ArrayList<>();
         ZooFactory.populateZoo(this);
     }
 
@@ -28,18 +26,6 @@ public class Zoo {
 
     public void setAnimals(ArrayList<Animal> animals) {
         this.animals = animals;
-    }
-
-    public ArrayList<Pen<Animal>> getPens() {
-        return pens;
-    }
-
-    public void setPens(ArrayList<Pen<Animal>> pens) {
-        this.pens = pens;
-    }
-
-    public void addPen(Pen<Animal> pen) {
-        pens.add(pen);
     }
 
     public void addAnimal(Animal animal) {
--- src/com/company/ZooFactory.java
+++ src/com/company/ZooFactory.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class ZooFactory {
 
-    private static Animal animalFactory(animalTypes animal, int index) {
+    private static Animal createAnimal(animalTypes animal, int index) {
         if (animalTypes.Lion.equals(animal)) {
             return new Lion("lion" + index);
         } else if (animalTypes.Llama.equals(animal)) {
@@ -14,31 +14,18 @@ public class ZooFactory {
         }
     }
 
+
     public static Zoo populateZoo(Zoo zoo) {
 
-        // RANDOM NUMBER OF ANIMALS?
-        Pen<Animal> lionPen = new Pen<>("lion");
-        Pen<Animal> llamaPen = new Pen<>("zebra");
-        Pen<Animal> crocodilePen = new Pen<>("crocodile");
-
         for (int i = 0; i < 10; i++) {
-            Animal lion = ZooFactory.animalFactory(animalTypes.Lion, i);
-            Animal llama = ZooFactory.animalFactory(animalTypes.Llama, i);
-            Animal crocodile = ZooFactory.animalFactory(animalTypes.Crocodile, i);
+            Animal lion = ZooFactory.createAnimal(animalTypes.Lion, i);
+            Animal llama = ZooFactory.createAnimal(animalTypes.Llama, i);
+            Animal crocodile = ZooFactory.createAnimal(animalTypes.Crocodile, i);
 
             zoo.addAnimal(lion);
-            lionPen.addAnimalToPen(lion);
-
             zoo.addAnimal(llama);
-            llamaPen.addAnimalToPen(lion);
-
             zoo.addAnimal(crocodile);
-            crocodilePen.addAnimalToPen(lion);
         }
-
-        zoo.addPen(lionPen);
-        zoo.addPen(llamaPen);
-        zoo.addPen(crocodilePen);
 
         return zoo;
     }
