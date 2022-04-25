7fb9cead8bda9ee0b8bfd7ef5a25565209755145
nologyRob <rob.hooper@nology.io>
2022-04-25 10:51:09 +0100

    Merge branch 'main' of https://github.com/nologyRob/NologyZooApplication into robs-copy-of-charlies # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/com/company/Zoo.java
+++ src/com/company/Zoo.java
@@ -11,18 +11,21 @@ import java.util.ArrayList;
 // - SEARCHING ANIMALS
 // ADD VISITORS IN ZOO FACTORY
 
-public class Zoo {
+public class Zoo{
 
     private final ArrayList<Animal> animals;
     private User currentUser;
     private ArrayList<Visitor> visitors;
+    private ArrayList<Zookeeper> zookeepers;
 //    private ArrayList<ZooKeeper> zooKeepers;
 
     public Zoo() {
         this.currentUser = null;
         this.animals = new ArrayList<>();
         this.visitors = new ArrayList<>();
+        this.zookeepers = new ArrayList<>();
         visitors.add(new Visitor("charlie", "test"));
+        zookeepers.add(new Zookeeper("rob", "test"));
         ZooFactory.populateZoo(this);
     }
 
@@ -31,6 +34,7 @@ public class Zoo {
     }
 
     public void addAnimal(Animal animal) {
+
         animals.add(animal);
     }
 
@@ -65,5 +69,17 @@ public class Zoo {
 
         return false;
     }
+    public boolean logInZookeeper(String name, String password) {
+        for (Zookeeper zookeeper : zookeepers) {
+            if (zookeeper.authenticate(name, password)) {
+                return true;
+            }
+        }
 
+        return false;
+    }
+
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
 }
